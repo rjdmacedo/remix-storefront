1f1d74bdbfb7704842932166e9e5c5e6839609d1
Rafael Macedo <rafaelmacedo4@gmail.com>
2023-05-18 14:52:20 +0100

    refactor: root.tsx and remix.config.js

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,11 @@
 import {type LinksFunction, type LoaderArgs} from '@shopify/remix-oxygen';
 import {
-  Links,
   Meta,
+  Links,
   Outlet,
   Scripts,
-  ScrollRestoration,
   useLoaderData,
+  ScrollRestoration,
 } from '@remix-run/react';
 import type {Shop} from '@shopify/hydrogen/storefront-api-types';
 import styles from './styles/app.css';
@@ -28,6 +28,7 @@ export const links: LinksFunction = () => {
 
 export async function loader({context}: LoaderArgs) {
   const layout = await context.storefront.query<{shop: Shop}>(LAYOUT_QUERY);
+
   return {layout};
 }
 
@@ -39,14 +40,12 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <Meta />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
         <Links />
       </head>
       <body>
-        <h1>Hello, {name}</h1>
-        <p>This is a custom storefront powered by Hydrogen</p>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
