ed3e549d90ed198ad8eefbb8c6139e85cd48ab0f
Rafael Macedo <rafaelmacedo4@gmail.com>
2023-05-18 15:23:58 +0100

    feat: create a hooks dir

---
+++ app/hooks/useDebounce.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function useDebounce<
+  Callback extends (...args: Parameters<Callback>) => ReturnType<Callback>,
+>(callback: Callback, delay: number) {
+  const callbackRef = React.useRef(callback);
+
+  React.useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  return React.useMemo(
+    () =>
+      debounce(
+        (...args: Parameters<Callback>) => callbackRef.current(...args),
+        delay,
+      ),
+    [delay],
+  );
+}
+
+function debounce<Callback extends (...args: Parameters<Callback>) => void>(
+  fn: Callback,
+  delay: number,
+) {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<Callback>) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
